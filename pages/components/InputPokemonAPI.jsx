3d2aedb0a9b0bf74a4089ae77b7b2723792286ba
Paula Cano <paulacanoroman@gmail.com>
2023-11-07 17:43:06 +0100

    feat: funcionamiento input con useState

--- pages/components/InputPokemonAPI.jsx
+++ pages/components/InputPokemonAPI.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const InputPokemonAPI = () => {
+
+    const [dataInputUser, setDataInputUser] = useState()
   return (
     <>
     <h3>Escribe el nombre de un pokémon</h3>
     <br></br>
-    <input></input>
+    <input onChange={(e)=>{setDataInputUser(e.target.value)}}></input>
+    <br></br>
+    {dataInputUser}
     <br></br>
     <h5>Por ejemplo: ditto, butterfree, pikachu, nidorina, pidgeot... </h5>
     </>
