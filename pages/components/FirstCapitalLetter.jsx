38081bee688aa2b43fc32cb0b65964750a86c3ae
Paula Cano <paulacanoroman@gmail.com>
2023-11-07 21:38:17 +0100

    feat: lógica cambiar primera letra mayúscula
    const goToWords = splitWords.map((oneWord)=>{
                const letter = oneWord.split("")
                const capitalLetter = letter.at(0).toUpperCase()
                letter[0] = capitalLetter
                return(
                    letter.join("")
                )
    
            })
            console.log(goToWords)

--- pages/components/FirstCapitalLetter.jsx
+++ pages/components/FirstCapitalLetter.jsx
@@ -2,10 +2,27 @@ import React, { useState } from 'react'
 
 const FirstCapitalLetter = () => {
     const [dataUser, setDataUser] = useState([])
+
+    const firstCapitalLetter = ()=>{
+        const copyArr = structuredClone(dataUser);
+        const splitWords = copyArr.split(" ");
+
+        const goToWords = splitWords.map((oneWord)=>{
+            const letter = oneWord.split("")
+            const capitalLetter = letter.at(0).toUpperCase()
+            letter[0] = capitalLetter
+            return(
+                letter.join("")
+            )
+            
+        })
+        console.log(goToWords)
+    }
   return (
     <>
     {dataUser}
     <input onChange={(e)=>{setDataUser(e.target.value)}}></input>
+    <button onClick={()=>{firstCapitalLetter()}}>convertir primera letra en mayúscula</button>
     </>
   )
 }
