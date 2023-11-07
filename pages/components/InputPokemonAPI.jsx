16f9c35056f09486212bcec4e46870c0ef89e027
Paula Cano <paulacanoroman@gmail.com>
2023-11-07 17:46:15 +0100

    feat: programacion defensiva esqueleto consumo API

--- pages/components/InputPokemonAPI.jsx
+++ pages/components/InputPokemonAPI.jsx
@@ -1,13 +1,25 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const InputPokemonAPI = () => {
 
-    const [dataInputUser, setDataInputUser] = useState()
+    const [dataInputUser, setDataInputUser] = useState();
+    const [dataAPI, setDataAPI] = useState([])
+
+    useEffect(() => {
+      try{
+
+
+      }
+      catch(error){"error deteceted", error}
+    }, [])
+    
+    // const handleShowPokemon = ()=>{}
   return (
     <>
     <h3>Escribe el nombre de un pokémon</h3>
     <br></br>
     <input onChange={(e)=>{setDataInputUser(e.target.value)}}></input>
+    <button onClick={()=>{}}>buscar</button>
     <br></br>
     {dataInputUser}
     <br></br>
