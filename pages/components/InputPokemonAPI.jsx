5001e3edeefc2785f6f04eea00fc9727e9b3098a
Paula Cano <paulacanoroman@gmail.com>
2023-11-07 17:41:51 +0100

    feat: esqueleto input y texto

--- pages/components/InputPokemonAPI.jsx
+++ pages/components/InputPokemonAPI.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const InputPokemonAPI = () => {
   return (
-    <div>InputPokemonAPI</div>
+    <>
+    <h3>Escribe el nombre de un pokémon</h3>
+    <br></br>
+    <input></input>
+    <br></br>
+    <h5>Por ejemplo: ditto, butterfree, pikachu, nidorina, pidgeot... </h5>
+    </>
   )
 }
 
